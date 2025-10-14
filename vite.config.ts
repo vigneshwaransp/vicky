bab30ed76fce2108d788d627dc7380656037e7b6
vigneshwaran <vigneshwaransp.cs24@bitsathy.ac.in>
2025-10-14 17:45:43 +0530

    Second commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
+  // import react from '@vitejs/plugin-react'
   import path from 'path';
 
   export default defineConfig({
     plugins: [react()],
+    base: '/portfolio/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
